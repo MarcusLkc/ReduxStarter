c471e66aa39c0cc0206171555446812e09c4d455
marcuslkc <marcuscrowder66@gmail.com>
2017-06-19 23:59:50 -0400

    Learned React State

---
+++ src/components/video_list_item.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+
+const VideoListItem = ({video}) => {
+	//const video = props.video;
+
+
+	return (
+	<li className="list-group-item">
+	<div className="video-list media">
+
+	</div>
+	</li>
+
+	);
+
+
+};
+
+
+export default VideoListItem;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+const API_KEY ='';
 
-// Create New Componenent to produce html
 
-const App = () => {
-	return <div>Hi!</div>;
+
+YTSearch({key: API_KEY, term: 'surfboards'}, function(data){
+	console.log(data);
+});
+
+// Create New Componenent to produce 
+
+
+class App extends Component{
+	constructor(props){
+		super(props);
+
+		this.state = {videos: []}
+
+		YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+			this.setState({ videos });
+
+			// this.setState({videos: videos})
+		});
+	}
+
+	render(){
+	return (<div> 
+				<SearchBar />
+				<VideoList videos={this.state.videos} />
+			</div>);
+	}
 }
 
 // Take this component's generate html and put it on the page
