14224ce7039c22f5cf88a18339d2e5816285a493
marcuslkc <marcuscrowder66@gmail.com>
2017-06-20 02:14:24 -0400

    Youtube Player finished

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 const API_KEY ='';
 
 
-
-YTSearch({key: API_KEY, term: 'surfboards'}, function(data){
-	console.log(data);
-});
 
 // Create New Componenent to produce 
 
@@ -18,21 +16,40 @@ class App extends Component{
 	constructor(props){
 		super(props);
 
-		this.state = {videos: []}
+		this.state = {
+			videos: [],
+			selectedVideo: null
+		};
 
-		YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
-			this.setState({ videos });
+		this.videoSearch('surfboards');
 
 			// this.setState({videos: videos})
+		
+	}
+
+	videoSearch(term) {
+		YTSearch({key: API_KEY, term: term}, (videos) => {
+			this.setState({ 
+				videos: videos,
+				selectedVideo: videos[0]
+			});
+
 		});
+
 	}
 
 	render(){
-	return (<div> 
-				<SearchBar />
-				<VideoList videos={this.state.videos} />
-			</div>);
-	}
+		const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 400);
+
+
+		return (<div> 
+					<SearchBar onSearchTermChange={videoSearch} />
+					<VideoDetail video={this.state.selectedVideo} />
+					<VideoList 
+					onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+					videos={this.state.videos} />
+				</div>);
+		}
 }
 
 // Take this component's generate html and put it on the page
